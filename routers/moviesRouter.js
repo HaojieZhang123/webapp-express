5b3b83ac0dd7a294c762fed989d9d7dbdfe25ac1
HaojieZhang123 <haojiezhang123@gmail.com>
2025-06-04 16:36:20 +0200

    [FEAT] installed, imported, configured and used multer (middleware and router)

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// multer
+const upload = require('../middlewares/multer');
+
 // controllers
 const moviesController = require('../controllers/moviesController');
 
 // routes
 router.get('/', moviesController.index);
 router.get('/:id', moviesController.show);
-router.post('/', moviesController.store);
+router.post('/', upload.single('image'), moviesController.store);
 router.put('/:id', moviesController.update);
 router.patch('/:id', moviesController.modify);
 router.delete('/:id', moviesController.destroy);
